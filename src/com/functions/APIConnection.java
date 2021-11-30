927b4d8ebd1f3ac129d147f7ace471074b98f821
pouparjea <77108740+poupardjea-07@users.noreply.github.com>
2021-11-30 17:13:06 +0100

    mise en place fichiers

---
+++ src/com/functions/APIConnection.java
@@ -0,0 +1,59 @@
+package com.functions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class APIConnection {
+	
+	private String URL_GLOBALE = "http://localhost:8182/villes";
+	private String URL_ONE_VILLE_CODE = "http://localhost:8182/villes/";
+	private String URL_ONE_VILLE_NOUN = "http://localhost:8182/villes/nom/";
+	
+
+	//Pour récup tous les enregistrements
+	public JSONArray getAllRows() throws IOException {
+		System.out.println("coucou");
+        URL url = new URL(URL_GLOBALE);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        InputStream inputStream = connection.getInputStream();
+        
+        final StringBuilder out = new StringBuilder(); 
+        final byte[] buffer = new byte[inputStream.available()]; 
+        try {
+            for (int ctr; (ctr = inputStream.read(buffer)) != -1;) {
+                out.append(new String(buffer, 0, ctr));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot convert stream to string", e);
+        }
+        
+        String result = out.toString();        
+       
+        JSONParser parser = new JSONParser();
+        JSONArray jsonArray = null;
+		try {
+			jsonArray = (JSONArray) parser.parse(result);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(jsonArray);
+		return jsonArray;
+	}
+	
+	public JSONObject getOneVille(String code) {
+		
+		return null;
+	}
+	
+
+	
+}
--- src/com/servlets/PagePrincipale.java
+++ src/com/servlets/PagePrincipale.java
@@ -1,9 +1,6 @@
 package com.servlets;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,9 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import com.functions.APIConnection;
 
 
 
@@ -21,7 +19,6 @@ import org.json.simple.parser.ParseException;
 @WebServlet("/PagePrincipale")
 public class PagePrincipale extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
     public PagePrincipale() {
         super();
@@ -32,55 +29,19 @@ public class PagePrincipale extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-        String myurl= "http://localhost:8182/villes/01001";
-
-        URL url = new URL(myurl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.connect();
-        InputStream inputStream = connection.getInputStream();
-        
-        final StringBuilder out = new StringBuilder(); 
-        final byte[] buffer = new byte[inputStream.available()]; 
-        try {
-            for (int ctr; (ctr = inputStream.read(buffer)) != -1;) {
-                out.append(new String(buffer, 0, ctr));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot convert stream to string", e);
-        }
-        
-        String result = out.toString();
-        System.out.println(result);
-        
-       
-        JSONParser parser = new JSONParser();
-        JSONObject jsonObject = null;
+		APIConnection apiConnection = new APIConnection();
+		JSONArray jsonArray = new JSONArray();
 		try {
-			jsonObject = (JSONObject) parser.parse(result);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
+			jsonArray = apiConnection.getAllRows();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
+		JSONObject jsonObject = (JSONObject) jsonArray.get(0);
         System.out.println(jsonObject);
-        System.out.println(jsonObject.get("nomCommune"));
         
-        /*
-        // On récupère le tableau d'objets qui nous concernent
-        JSONArray array = new JSONArray(jsonObject.getString("personnes"));
-        // Pour tous les objets on récupère les infos
-        for (int i = 0; i < array.length(); i++) {
-            // On récupère un objet JSON du tableau
-            JSONObject obj = new JSONObject(array.getString(i));
-            // On fait le lien Personne - Objet JSON
-            Personne personne = new Personne();
-            personne.setNom(obj.getString("nom"));
-            personne.setPrenom(obj.getString("prenom"));
-            // On ajoute la personne à la liste
-            personnes.add(personne);
-           
-        }*/
-		
-		
+        
 		this.getServletContext().getRequestDispatcher("/WEB-INF/pagePrincipale.jsp").forward(request, response);
 	}
 
